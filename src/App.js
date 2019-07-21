e377330d329e6134cfe3742d8428bf2c07ba081d
Emily Zhang <emily.aiwen@gmail.com>
2019-07-21 13:05:44 -0700

    Added style and meme search

--- src/App.js
+++ src/App.js
@@ -1,33 +1,43 @@
 import React from 'react';
 import './App.css';
-import key from './api.txt'
+import { FiSearch } from "react-icons/fi";
 
 class App extends React.Component {
 
-  state={
-    memes:[],
-    loading:false,
-    text:''
+  state = {
+    memes: [],
+    loading: false,
+    text: ''
   }
 
   getMemes = async (e) => {
     e.preventDefault()
-    this.setState({loading: true})
-    var apiKey = key
+    this.setState({ loading: true })
+    var key = 'BFZynaGtycMTKfG606H1SHrTrfZ3FogR'
     var url = `http://api.giphy.com/v1/gifs/search?q=${this.state.text}&api_key=${key}`
+    var r = await fetch(url)
+    var json = await r.json()
+    this.setState({ memes: json.data, loading: false, text: '' })
   }
 
   render() {
-    var {memes, loading, text} = this.state
+    var { memes, loading, text } = this.state
     return (
       <div className="App">
         <form className="App-header" onSubmit={this.getMemes}>
-          <input value={text}
-            onChange={e=> this.setState({text: e.target.value})}/>
-          <button disabled={loading || !text} type='submit'>
-            Search
-          </button>
+          <input className='searchbar'
+            value={text}
+            placeholder='Search'
+            onChange={e => this.setState({ text: e.target.value })} />
+          <FiSearch className='button'
+            disabled={loading || !text}
+            type='submit' />
         </form>
+          <main className='main'>
+            {memes.map(meme => {
+              return <Meme key={meme.id} meme={meme} />
+            })}
+          </main>
       </div>
     );
   }
@@ -35,3 +45,11 @@ class App extends React.Component {
 
 
 export default App;
+
+function Meme(props) {
+  const { meme } = props
+  const url = meme.images.fixed_height.url
+  return (<div className="meme-wrap" onClick={() => window.open(url, '_blank')}>
+    <img height="200" alt="meme" src={url} />
+  </div>)
+}
